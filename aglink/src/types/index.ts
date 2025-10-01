c09561ec57f5b0fefb3abff9b39985d376505bfc
1F10230077 <s1F102300773@iniad.org>
2025-10-01 16:11:25 +0900

    index.tsの農業タイプを更新、ユーザーのプロフィールやログイン用の型を作成しました。

--- aglink/src/types/index.ts
+++ aglink/src/types/index.ts
@@ -1,13 +1,34 @@
 export type Farm = {
   id: string;
   name: string;
-  type: "週末農業タイプ" | "家庭菜園タイプ";
+  type: "AFHO" | "AFHP - のんびり農業タイプ" | "AFIO" | "AFIP" |
+        "ACHO" | "ACHP" | "ACIO" | "ACIP" |
+        "SFHO" | "SFHP" | "SFIO" | "SFIP" |
+        "SCHO" | "SCHP" | "SCIO" | "SCIP";
   location?: string;
   description: string;
   imageUrl: string;
 };
 
 export type Diagnosis = {
-  type: "週末農業タイプ" | "家庭菜園タイプ";
+  type: "AFHO" | "AFHP - のんびり農業タイプ" | "AFIO" | "AFIP" |
+        "ACHO" | "ACHP" | "ACIO" | "ACIP" |
+        "SFHO" | "SFHP" | "SFIO" | "SFIP" |
+        "SCHO" | "SCHP" | "SCIO" | "SCIP";
   description: string;
+};
+
+export type Profile = {
+  id: string; //DBから提供されるID
+  username: string;
+};
+
+export type LoginInput = {
+  username: string;
+  password: string;
+};
+
+export type SignUpInput = {
+  username: string;
+  password: string;
 };
