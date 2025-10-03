2287ba36076ed658d06bae784177793a999aa990
1F10230077 <s1F102300773@iniad.org>
2025-10-03 17:00:07 +0900

    fix: 農業タイプの名前を質問案のPDFに基づいて修正

--- aglink/src/types/index.ts
+++ aglink/src/types/index.ts
@@ -1,20 +1,20 @@
 export type Farm = {
   id: string;
   name: string;
-  type: "AFHO" | "AFHP - のんびり農業タイプ" | "AFIO" | "AFIP" |
-        "ACHO" | "ACHP" | "ACIO" | "ACIP" |
-        "SFHO" | "SFHP" | "SFIO" | "SFIP" |
-        "SCHO" | "SCHP" | "SCIO" | "SCIP";
+  type: "AFHO - 週末ガーデナータイプ" | "AFHP - のんびり家庭菜園タイプ" | "AFIO - ロマンティックファーマータイプ" | "AFIP - 週末研究家タイプ" |
+        "ACHO - アグリインフルエンサータイプ" | "ACHP - 職人気質ガーデナータイプ" | "ACIO - 企業家アグリタイプ" | "ACIP - アーバンファーマータイプ" |
+        "SFHO - ソーシャルアグリタイプ" | "SFHP - 堅実な家庭菜園タイプ" | "SFIO - 理系アグリタイプ" | "SFIP - ロジカル農家タイプ" |
+        "SCHO - 週末チャレンジファーマータイプ" | "SCHP - プロフェッショナル農家タイプ" | "SCIO - テクノロジー農家タイプ" | "SCIP - 職人ファーマータイプ";
   location?: string;
   description: string;
   imageUrl: string;
 };
 
 export type Diagnosis = {
-  type: "AFHO" | "AFHP - のんびり農業タイプ" | "AFIO" | "AFIP" |
-        "ACHO" | "ACHP" | "ACIO" | "ACIP" |
-        "SFHO" | "SFHP" | "SFIO" | "SFIP" |
-        "SCHO" | "SCHP" | "SCIO" | "SCIP";
+  type: "AFHO - 週末ガーデナータイプ" | "AFHP - のんびり家庭菜園タイプ" | "AFIO - ロマンティックファーマータイプ" | "AFIP - 週末研究家タイプ" |
+        "ACHO - アグリインフルエンサータイプ" | "ACHP - 職人気質ガーデナータイプ" | "ACIO - 企業家アグリタイプ" | "ACIP - アーバンファーマータイプ" |
+        "SFHO - ソーシャルアグリタイプ" | "SFHP - 堅実な家庭菜園タイプ" | "SFIO - 理系アグリタイプ" | "SFIP - ロジカル農家タイプ" |
+        "SCHO - 週末チャレンジファーマータイプ" | "SCHP - プロフェッショナル農家タイプ" | "SCIO - テクノロジー農家タイプ" | "SCIP - 職人ファーマータイプ";
   description: string;
 };
 
